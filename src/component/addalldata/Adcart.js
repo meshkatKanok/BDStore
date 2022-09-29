60bf2ffcb033d72aa6d4a9cf99cf62c627a71107
meshkatKanok <meshkatkanok12@gmail.com>
2022-09-29 19:37:35 +0600

    localstorage

--- src/component/addalldata/Adcart.js
+++ src/component/addalldata/Adcart.js
@@ -8,7 +8,7 @@ import { useState } from 'react';
 
 
 const Adcart = (props) => {
-  const notify = () => toast(" Wow!Congratulation Complate Your Order");
+  // const notify = () => toast(" Wow!Congratulation Complate Your Order");
   const [add, setAdd] = useState(0);
   const saveLocaldata = localStorage.getItem("breakAdd")
   const HandleaddClick = (ok) => {
